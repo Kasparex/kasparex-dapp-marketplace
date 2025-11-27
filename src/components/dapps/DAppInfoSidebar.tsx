a5c03882d10568373381d4b19f003dc5ba463612
Kasparex <patreontv@gmail.com>
2025-11-27 16:43:45 +0100

    Fix missing useEffect import in DAppInfoSidebar

--- src/components/dapps/DAppInfoSidebar.tsx
+++ src/components/dapps/DAppInfoSidebar.tsx
@@ -1,6 +1,6 @@
 'use client';
 
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
 import { useAccount, useChainId } from 'wagmi';
 import { useChainModal } from '@rainbow-me/rainbowkit';
 import { ConnectButton } from '@rainbow-me/rainbowkit';
